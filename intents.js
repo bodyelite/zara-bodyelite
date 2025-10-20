78e0f86c8c589a53569a2ae8ad46b89e7137a3fe
Juan Contreras <juan@bodyelite.cl>
2025-10-20 18:52:18 -0300

    Actualización intents y responses de Zara IA

---
+++ intents.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { tag: 'saludo', patterns: ['hola','buenas','hey','holaa','holis','buen día','buenas tardes'], response: 'saludo' },
+  { tag: 'facial', patterns: ['facial','faciales','rostro','cara','piel','tratamiento facial'], response: 'facial' },
+  { tag: 'corporal', patterns: ['corporal','cuerpo','abdomen','piernas','brazos','glúteos','tratamiento corporal'], response: 'corporal' },
+  { tag: 'flacidez', patterns: ['flacidez','piel suelta','firmeza','tonificar','piel caída'], response: 'flacidez' },
+  { tag: 'lipo', patterns: ['lipo','grasa','reducción','reductiva','cavitación','abdomen','cintura'], response: 'lipo' },
+  { tag: 'sesiones', patterns: ['sesión','sesiones','cuántas','duración','cuanto dura'], response: 'sesiones' },
+  { tag: 'agendar', patterns: ['agendar','reserva','agenda','evaluación','cita','hora'], response: 'agendar' }
+];
