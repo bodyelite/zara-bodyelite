ae20d72222d4c0b883a6ada279fe16576e620eb4
Juan Contreras <juan@bodyelite.cl>
2025-12-26 16:06:29 -0300

    Config: Update 4 Staff Alert Numbers

--- src/config/business.js
+++ src/config/business.js
@@ -1,6 +1,14 @@
 export const NEGOCIO = {
     nombre: "Body Elite",
     agenda_link: "https://agendamiento.reservo.cl/makereserva/agenda/f0Hq15w0M0nrxU8d7W64x5t2S6L4h9",
-    staff_alertas: ["56961922377", "56987654321"], 
+    
+    // 🚨 LISTA DE ALERTA (LOS 4 NÚMEROS QUE ME DISTE)
+    staff_alertas: [
+        "56983300262",
+        "56955145504",
+        "56937648536",
+        "56931720760"
+    ], 
+    
     direccion: "Av. Las Perdices Nº2990, Peñalolén"
 };
