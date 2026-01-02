b48c087a7d88191374f4d6478f64aaeb7519b0d2
Juan Contreras <juan@bodyelite.cl>
2026-01-02 17:02:18 -0300

    ZARA: Corregida micro a D17v

--- src/config/business.js
+++ src/config/business.js
@@ -3,5 +3,6 @@ export const NEGOCIO = {
     agenda_link: "https://agendamiento.reservo.cl/makereserva/agenda/f0Hq15w0M0nrxU8d7W64x5t2S6L4h9",
     staff_alertas: ["56983300262", "56955145504", "56937648536"], 
     direccion: "Av. Las Perdices 2990, Local 23, Peñalolén (Strip Center Las Perdices)",
+    ubicacion_detalle: "Desde Metro Quilín (Línea 4) 🚇, puedes tomar la micro D17v 🚌 hacia el oriente. Nos bajamos justo en el Strip Center Las Perdices.",
     hora_limite_seguimiento: 21
 };
