ba02ec4406bcc98d1ae583e5c0c5817b011fcb11
Juan Contreras <juan@bodyelite.cl>
2026-02-09 21:57:43 -0300

    Limpieza total de archivos duplicados y basura

--- src/config/Copia de business.js
+++
@@ -1,8 +0,0 @@
-export const NEGOCIO = {
-    nombre: "Body Elite",
-    agenda_link: "https://agendamiento.reservo.cl/makereserva/agenda/f0Hq15w0M0nrxU8d7W64x5t2S6L4h9",
-    staff_alertas: ["56983300262", "56955145504", "56937648536"], 
-    direccion: "Av. Las Perdices 2990, Local 23, Peñalolén (Strip Center Las Perdices)",
-    ubicacion_detalle: "Desde Metro Quilín (Línea 4) 🚇, puedes tomar la micro D17v 🚌 hacia el oriente. Nos bajamos justo en el Strip Center Las Perdices.",
-    hora_limite_seguimiento: 21
-};
